5131d1782b74e8ae9156dba01e20971928047f97
Jose Seie <joseeugenioseie@hotmail.com>
2018-10-06 00:19:20 +0200

    Update lemp-ubuntu-instalation.sh

--- lemp-ubuntu-instalation.sh
+++ lemp-ubuntu-instalation.sh
@@ -26,9 +26,9 @@ sudo apt install nginx;
 # Allowing trafic in 80 and 443 ports
 sudo ufw allow 'Nginx HTTP';
 sudo ufw allow 'Nginx HTTPS';
+sudo ufw allow 'OpenSSH';
+sudo ufw enable;
 
-# Restart nginx
-service nginx restart;
 
 # Instalacao do mySql
 sudo apt install mysql-server-5.7;
@@ -36,8 +36,93 @@ sudo apt install mysql-server-5.7;
 # Instalacao do php
 sudo apt install php-fpm php-mysql;
 
-# Restart nginx
-sudo systemctl reload nginx;
+# Instalacao dos repositorios de terceiros,...
+sudo apt install software-properties-common;
+sudo add-apt-repository ppa:ondrej/php;
+apt-get install python-software-properties;
+
+# Instalacao de php 7.2
+sudo apt update;
+sudo apt install php7.2-fpm;
+
+sudo apt-get install php7.2-cli php7.2-fpm php7.2-curl php7.2-gd php7.2-mysql php7.2-mbstring zip unzip;
+
+apt-get update;
+apt-get -y install unzip zip php7.2 php7.2-mysql php7.2-fpm php7.2-mbstring php7.2-xml php7.2-curl php7.2-xml php-dev php-pear libmcrypt-dev;
+
+# ====================================================
+# Definindo PHP 7.2 como default
+# ====================================================
+
+update-alternatives --set php /usr/bin/php7.2;
+
+# ====================================================
+# Instalacao do PHP myAdmin
+# ====================================================
+
+sudo apt-get update;
+sudo apt-get install phpmyadmin;
+
+# Criando o link
+sudo ln -s /usr/share/phpmyadmin /var/www/html;
+
+# ====================================================
+# Reiniciando nginx
+# ====================================================
+
+echo copiando p ficheiro de configurações;
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bk;
+sudo rm /etc/nginx/sites-available/default;
+sudo cp default /etc/nginx/sites-availables/;
+
+# =====================================================
+# Instalacao do composer
+# =====================================================
+
+cd ~;
+curl -sS https://getcomposer.org/installer | php;
+sudo mv composer.phar /usr/local/bin/composer;
+
+# =====================================================
+# Instalacao do projecto Laravel para testes
+# =====================================================
+echo "Asseguir vai criar o directório /var/www/laravel e criar um Projecto laravel com o nome test"
+# Criacao de pasta Laravel no /var/www/
+mkdir -p /var/www/laravel;
+
+# Criar um novo projecto Laravel para testes
+sudo composer create-project laravel/laravel /var/www/laravel/test;
+
+
+# ===> Permissões ao directório d Projecto:
+sudo chown -R :www-data /var/www/laravel/test;
+sudo chmod -R 775 /var/www/laravel/test/storage;
+sudo chmod -R 775 /var/www/laravel/test/bootstrap/cache;
+
+# Clonando o projecto explicador do github
+echo Agora vamos clonar o projecto que deseja hospedar no github;
+echo link do repositório:;
+read repo;
+echo colocar no directorio?:
+read dir;
+sudo git clone $repo /var/www/laravel/$dir;
+
+# Permissoes para o Projecto explicador (actualize aqui para o seu projecto especifico)
+sudo chown -R :www-data /var/www/laravel/$dir
+sudo chmod -R 775 /var/www/laravel/$dir/storage
+sudo chmod -R 775 /var/www/laravel/$dir/bootstrap/cache;
+
+# ====================================================
+# Reiniciando nginx
+# ====================================================
+
+sudo service php7.2-fpm restart;
+sudo service nginx reload;
+sudo service nginx restart;
+
+# =====================================================
+# Informacoes de teste do servidor
+# =====================================================
 
 # Criando o ficheiro de teste de instalacao de PHP
 echo "<?php" > /var/www/html/info.php;
@@ -47,5 +132,47 @@ echo "phpinfo();" >> /var/www/html/info.php;
 echo;
 echo;
 echo Instalação concluida com sucesso!!;
+echo NOTA 1: Não esquece de actualizar o 'server name', no: /etc/nginx/sites-availables/default;
+echo NOTA 2: Não esquece de definir permissões de Leitura ao seu projecto Laravel;
+echo NOTA 3: Use este comando: sudo nano /etc/php/7.2/fpm/php.ini;
+echo "Descomente e atribui valor zero (0) na linha 'cgi.fix_pathinfo' como vês asseguir: ";
+echo cgi.fix_pathinfo=0;
+echo Depois reiniciar: sudo service php7.0-fpm restart;
+echo;
 echo Pode testar o servidor da seguinte forma:;
 echo "http://your_server_domain_or_IP/info.php";
+echo Esse é o seu ip:;
+curl -4 icanhazip.com;
+
+# ==========================================
+# === Notas
+# ==========================================
+# sudo nano /etc/php/7.0/fpm/php.ini
+#
+# Definir este valor e descomentar a linha
+#	
+# cgi.fix_pathinfo=0
+
+# Restart PHP 7.2 FPM
+# sudo service php7.2-fpm restart
+
+#  =============================================
+#  Base de dados
+# ==============================================
+#
+# Criacao da base de dados
+# CREATE DATABASE explicadordb2018;
+# Criacao de User
+# CREATE USER 'explicador2018'@'localhost' IDENTIFIED BY 'new_password';
+
+# Permissoes na db
+# GRANT ALL ON explicadordb2018.* TO 'new_user'@'localhost';
+
+# Actualizar as permissoes
+# FLUSH PRIVILEGES;
+#
+# Para ver base de dados:
+# SHOW DATABASES;
+
+
+
