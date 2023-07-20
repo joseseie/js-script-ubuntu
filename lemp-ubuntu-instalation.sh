24a4c35871d6118e5546fec8606a3d8b7d51c02c
Jose Seie <joseeugenioseie@hotmail.com>
2023-07-20 19:31:34 +0200

    Update lemp-ubuntu-instalation.sh
    php 8.2 e suas extensões

--- lemp-ubuntu-instalation.sh
+++ lemp-ubuntu-instalation.sh
@@ -68,6 +68,14 @@ sudo apt-get install php8.1-cli php8.1-fpm php8.1-curl php8.1-gd php8.1-mysql ph
 
 sudo apt -y install unzip zip php8.1 php8.1-mysql php8.1-fpm php8.1-mbstring php8.1-xml php8.1-curl php-dev php-pear libmcrypt-dev;
 
+# Instalacao de php 8.2
+sudo apt update;
+sudo apt install php8.2-fpm;
+
+sudo apt-get install php8.2-cli php8.2-fpm php8.2-curl php8.2-gd php8.2-mysql php8.2-mbstring zip unzip;
+
+sudo apt -y install unzip zip php8.2 php8.2-mysql php8.2-fpm php8.2-mbstring php8.2-xml php8.2-curl php-dev php-pear libmcrypt-dev;
+
 
 # ====================================================
 # Definindo PHP 7.4 como default
