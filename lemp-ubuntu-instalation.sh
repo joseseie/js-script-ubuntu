2ea4728e4f22652010f696e0a0415e498387bf65
Jose Seie <joseeugenioseie@hotmail.com>
2023-12-19 21:36:36 +0200

    Update lemp-ubuntu-instalation.sh

--- lemp-ubuntu-instalation.sh
+++ lemp-ubuntu-instalation.sh
@@ -45,34 +45,26 @@ sudo add-apt-repository ppa:ondrej/php;
 
 # Instalacao de php 7.4
 sudo apt update;
-sudo apt install php7.4-fpm;
-
-sudo apt-get install php7.4-cli php7.4-fpm php7.4-curl php7.4-gd php7.4-mysql php7.4-mbstring zip unzip;
+sudo apt install php7.4-fpm php7.4-cli php7.4-curl php7.4-gd php7.4-mysql php7.4-mbstring zip unzip;
 
 sudo apt update;
 sudo apt -y install unzip zip php7.4 php7.4-mysql php7.4-fpm php7.4-mbstring php7.4-xml php7.4-curl php7.4-xml php-dev php-pear libmcrypt-dev;
 
 # Instalacao de php 8.0
 sudo apt update;
-sudo apt install php8.0-fpm;
-
-sudo apt-get install php8.0-cli php8.0-fpm php8.0-curl php8.0-gd php8.0-mysql php8.0-mbstring zip unzip;
+sudo apt install php8.0-fpm php8.0-cli php8.0-curl php8.0-gd php8.0-mysql php8.0-mbstring zip unzip;
 
 sudo apt -y install unzip zip php8.0 php8.0-mysql php8.0-fpm php8.0-mbstring php8.0-xml php8.0-curl php8.0-xml php-dev php-pear libmcrypt-dev;
 
 # Instalacao de php 8.1
 sudo apt update;
-sudo apt install php8.1-fpm;
-
-sudo apt-get install php8.1-cli php8.1-fpm php8.1-curl php8.1-gd php8.1-mysql php8.1-mbstring zip unzip;
+sudo apt install php8.1-fpm php8.1-cli php8.1-curl php8.1-gd php8.1-mysql php8.1-mbstring zip unzip;
 
 sudo apt -y install unzip zip php8.1 php8.1-mysql php8.1-fpm php8.1-mbstring php8.1-xml php8.1-curl php-dev php-pear libmcrypt-dev;
 
 # Instalacao de php 8.2
 sudo apt update;
-sudo apt install php8.2-fpm;
-
-sudo apt-get install php8.2-cli php8.2-fpm php8.2-curl php8.2-gd php8.2-mysql php8.2-mbstring zip unzip;
+sudo apt install php8.2-fpm php8.2-cli php8.2-curl php8.2-gd php8.2-mysql php8.2-mbstring zip unzip;
 
 sudo apt -y install unzip zip php8.2 php8.2-mysql php8.2-fpm php8.2-mbstring php8.2-xml php8.2-curl php-dev php-pear libmcrypt-dev;
 
